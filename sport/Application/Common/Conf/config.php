3845269c1390d1453e6147c0d708b104a04abd41
yann <312910815@qq.com>
2016-06-05 15:16:28 +0800

    config.php

--- sport/Application/Common/Conf/config.php
+++ sport/Application/Common/Conf/config.php
@@ -1,4 +1,13 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'DB_TYPE'  =>'mysql',
+    'DB_HOST'  =>'localhost',
+    'DB_NAME'  =>'sport',
+    'DB_USER'  =>'yann',
+    'DB_PWD'   =>'500239',
+    'DB_PORT'  =>'3306',
+    'DB_PREFIX'=>'sport_',
+    'SHOW_PAGE_TRACE' =>true, //开启trace
+
 );
