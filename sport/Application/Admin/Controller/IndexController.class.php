571aabf77ece8ef9c79b5508277a809aa0f0e56d
fuckingsourcecode <letitbemexiangwenjie@outlook.com>
2016-06-12 17:02:27 +0800

    个人信息

--- sport/Application/Admin/Controller/IndexController.class.php
+++ sport/Application/Admin/Controller/IndexController.class.php
@@ -2,13 +2,7 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function check()
-    {
-        if(!isset($_SESSION['username'])||$_SESSION['username']=='')
-        {
-            $this->redirect();
-        }
-    }
+    
     public function index()
     {
       $this->display();
---
+++ sport/Application/Admin/Controller/ManageStuController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class ManageStuController extends Controller{
+	public function login()
+	{
+	   $this->display();
+	}
+} 
