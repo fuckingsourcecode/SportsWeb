c9d7fa28bad5c0bbe466a2c1df07c350f7455ab0
fuckingsourcecode <letitbemexiangwenjie@outlook.com>
2016-06-13 19:25:49 +0800

    inital admin

--- sport/Application/Home/Controller/PersonalController.class.php
+++ sport/Application/Home/Controller/PersonalController.class.php
@@ -8,9 +8,13 @@ class PersonalController extends Controller{
     {
         $m=new Model();
         $m->query('set names utf8');
-        $name=$_SESSION['name'];
-        $sql="select * from `sport_user` WHERE name='$name'";
+        $id=$_SESSION['id'];
+        $sql="select * from `sport_user`  WHERE id='$id'";
         $array=$m->query($sql);
+
+        $sql1="select project from `sport_player`  WHERE id='$id'";
+        $project=$m->query($sql1);
+        $this->assign('project',$project);
         $this->assign('array',$array);
         $this->display();
     }
